22bad7b6f06df89e5195a23f7567841cde5e113d
MinSang22Kim <129925473+MinSang22Kim@users.noreply.github.com>
2023-08-05 00:12:09 +0900

    Update login_action.php

--- login_action.php
+++ login_action.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$connect = mysqli_connect("20.196.222.1", "root", "alstkd9031", "realMSDB") or die("connect failed");
+$connect = mysqli_connect("20.196.222.1", "ms", "alstkd9031", "realMSDB") or die("connect failed");
 
 //입력 받은 id와 password
 $id = $_POST['id'];
